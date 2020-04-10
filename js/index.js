d56af543cbf1ea40c6866cf6eb7536f93063e50e
Syrax <teagankeith@outlook.com>
2020-04-09 22:03:26 -0400

    1/10

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 // Your code goes here
 
-let titleText = document.querySelector(.logo-heading);
+let titleText = document.querySelector(".logo-heading");
+
+titleText.addEventListener("click", () => {
+   if(titleText.style.color === "rebeccapurple") {
+       titleText.style.color = "black"
+   } else {
+       titleText.style.color = "rebeccapurple"
+   }
+})
+
+
+
 
 
